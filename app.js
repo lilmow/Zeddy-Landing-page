bd3d34ecd3ff091b44b68979d0346795129bc621
lilmow <105414320+lilmow@users.noreply.github.com>
2023-05-13 02:38:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,18 +15,6 @@ const faqQuestions = document.querySelectorAll(".faq_question");
         });
     });
 
-    const mobileBtn = document.getElementById('mobile-cta')
-    nav = document.querySelector('nav')
-    mobileBtnExit = document.getElementById('mobile-exit');
-
-mobileBtn.addEventListener('click', () => {
-  nav.classList.add('menu-btn');
-})
-
-mobileBtnExit.addEventListener('click', () => {
-  nav.classList.remove('menu-btn');
-})
-
 
 
     
